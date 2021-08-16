93c4836f62fb543b0fa8dd602bb1b20f54036f4a
Maxim Predko <maxpredko@gmail.com>
2021-08-16 14:05:11 +0300

    feat: write function for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '')
+  
+  let stack = []
+  for (let bracket of str) {
+      let bracketsInd = brackets.indexOf(bracket)
+
+      if (bracketsInd % 2 === 0) {
+                
+        if (bracket === brackets[bracketsInd + 1] && stack[stack.length - 1] === bracketsInd){
+            stack.pop();
+        } else if (bracket === brackets[bracketsInd + 1] && stack[stack.length - 1] !== bracketsInd) {
+            stack.push(bracketsInd)
+        }
+        else{
+            stack.push(bracketsInd)
+        }
+    } 
+    else {
+        if (stack.pop() !== bracketsInd-1){
+            return false
+        }
+    }
+    
+}
+return stack.length === 0
 }
